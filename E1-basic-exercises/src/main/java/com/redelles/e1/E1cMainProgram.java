2161a9645776fb3c316c94eb45aa4320a95e5b87
Javier Garcia Muñoz <javidesoft@gmail.com>
2020-08-04 21:07:44 +0200

    Odd exercises

---
+++ E1-basic-exercises/src/main/java/com/redelles/e1/E1cMainProgram.java
@@ -0,0 +1,16 @@
+package com.redelles.e1;
+
+import java.io.IOException;
+
+/**
+ * E1c. Input from keyboard two numbers and show the odd numbers between them.
+ */
+public class E1cMainProgram {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Title");
+        System.out.println("Introduce the first number");
+        //You should search in internet to see how to input numbers from keyboard.
+        System.out.println("Introduce the second number");
+    }
+}
