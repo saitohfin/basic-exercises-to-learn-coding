cb2998d8b80b0dd9ed8ffbea708f033ed3f3aad7
Adrian M.A <adrian@neurocadi.es>
2020-09-07 17:18:58 +0200

    Exercise 3 solved in Ex.2 BreakdownMoneyMainProgramD

--- E2-Breakdown-Money/src/main/java/com/redelles/e2/GetNumbers.java
+++ E2-Breakdown-Money/src/main/java/com/redelles/e2/GetNumbers.java
@@ -54,4 +54,58 @@ public class GetNumbers {
         return quantities;
     }
 
+    public static Double[] getRandomDoubleNumberFrom1To10(){
+
+        Double[] arrayNumbers = new Double[5];
+        arrayNumbers[0] = Math.floor(Math.random()*(10-1+1)+1);
+        arrayNumbers[1] = Math.floor(Math.random()*(10-1+1)+1);
+        arrayNumbers[2] = Math.floor(Math.random()*(10-1+1)+1);
+        arrayNumbers[3] = Math.floor(Math.random()*(10-1+1)+1);
+        arrayNumbers[4] = Math.floor(Math.random()*(10-1+1)+1);
+
+        return arrayNumbers;
+    }
+
+    public static Double[] GetSortedAscArray(Double[] arrayOfNumbers){
+
+        for (int i = 0; i <arrayOfNumbers.length; i++) {
+            for (int j = i+1; j <arrayOfNumbers.length; j++) {
+                if(arrayOfNumbers[i] >arrayOfNumbers[j]) {
+                    Double temp = arrayOfNumbers[i];
+                    arrayOfNumbers[i] = arrayOfNumbers[j];
+                    arrayOfNumbers[j] = temp;
+                }
+            }
+        }
+
+        return arrayOfNumbers;
+    }
+
+    public static Double GetMaximumFromArray(Double[] arrayOfNumbers){
+
+        Double max = arrayOfNumbers[0];
+        for(Integer i = 1; i < arrayOfNumbers.length; i++){
+            if(max < arrayOfNumbers[i]){
+                max = arrayOfNumbers[i];
+            }
+        }
+
+        return max;
+    }
+
+    public static Double GetMinimumFromArray(Double[] arrayOfNumbers){
+
+        Double min = arrayOfNumbers[0];
+        for(Integer i = 1; i < arrayOfNumbers.length; i++){
+            if(min > arrayOfNumbers[i]){
+                min = arrayOfNumbers[i];
+            }
+        }
+
+        return min;
+    }
+
+
+
+
 }
---
+++ E3-SortArray/src/main/java/com/exercise3/ArrayExercise3.java
@@ -0,0 +1,10 @@
+package com.exercise3;
+
+import java.io.IOException;
+
+public class ArrayExercise3 {
+
+    public static void main (String[] args) throws IOException {
+        System.out.println("hola");
+    }
+}
